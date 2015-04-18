a226aa2d4b8bea143941bb1dc8882865677bd228
ws23 <linyutsu23@gmail.com>
2015-04-18 20:30:38 +0800

    change about css

--- about.php
+++ about.php
@@ -1,16 +1,70 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>愛奇藝股份有限公司</title>
-	<meta name="title" content="愛奇藝" />
-	<link type="text/css" rel="stylesheet" href="index.css" />
-<?php
-	require_once('header.php'); 
-	setLog($DBmain, 'info', 'into index', ''); 
-	$now = date('Y-m-d H:i:s', time()); 
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="title" content"愛奇藝臺灣官方網站">
+    <meta name="description" content="">
+    <meta name="author" content="臺灣愛奇藝股份有限公司">
 
+    <title>愛奇藝臺灣官方網站</title>
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="index.css" rel="stylesheet">
+	<script src="jquery/jquery-1.11.2.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+
+	<?php require_once('std.php'); ?> 
+  </head>
+
+  <body class="outliner">
+	<!-- preprocess start-->
+	<div id="fb-root"></div>
+
+	<script>
+	    (function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		    if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			    js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=748904491889773&version=v2.0";
+				  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+
+<?php
+	setLog($DBmain, 'info', 'into index', ''); 
+	$now = date('Y-m-d H:o:s', time());
+	
 	require_once('updateState.php'); 
 ?>
+	<!-- preprocess end -->
+
+
+	<!-- header start-->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img class="logo" src="img/logo.png"/></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">愛奇藝臺灣官方網站</a></li>
+           <!-- <li><a href="#"></a></li>
+            <li><a href="#"></a></li>-->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+	<!-- header end -->
+
 <!-- 首頁內容 start -->
 <div class="body">
     <div class="body-about">

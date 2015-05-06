86c9a98cdb721501e4f618fdcc5d04140620cf58
ws23 <linyutsu23@gmail.com>
2015-05-06 20:12:46 +0800

    15-5-6

--- register/index.php
+++ register/index.php
@@ -1,4 +1,15 @@
-<?php 
-	header('location: https://docs.google.com/forms/d/1WkfKWUET3aktXnbisfIoXj-OTs0McqgIujMAEXmQuuY/viewform'); 
-	exit; 
-?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<frameset cols="0%,100%" framespacing="0" border="0">
+	<frame frameborder=0 noresize>
+	<frame src="https://docs.google.com/forms/d/1WkfKWUET3aktXnbisfIoXj-OTs0McqgIujMAEXmQuuY/viewform" frameborder=0>
+	<noframes>
+		<script language="javascript">
+			window.location.href="https://docs.google.com/forms/d/1WkfKWUET3aktXnbisfIoXj-OTs0McqgIujMAEXmQuuY/viewform"; 
+		</script>
+	</noframes>
+</frameset>
+</html>
